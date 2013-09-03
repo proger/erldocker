ebe84a74b973ab605bc6acdd9e63c89d99d79abc
Vladimir Kirillov <proger@wilab.org.ua>
2013-09-03 19:39:28 +0300

    implemented container api

--- src/erldocker.erl
+++ src/erldocker.erl
@@ -1,27 +1,105 @@
 -module(erldocker).
 -compile([export_all]).
 
-% @doc Similar to the docker build command.
-% Either path or fileobj needs to be set.
-% path can be a local path (To a directory containing a Dockerfile) or a remote URL.
-% fileobj must be a readable file-like object to a Dockerfile.
-build([{path, undefined}, {tag, undefined}, {quiet, false}, {nocache, false}] = Args) -> {error, not_implemented}.
+%
+% Misc
+% 
 
-% @doc Identical to the docker commit command.
-commit(Container, [{repository, undefined}, {tag, undefined}, {message, undefined}, {author, undefined}, {conf, undefined}] = Args) -> {error, not_implemented}.
+% @doc Identical to the docker info command.
+info() -> erldocker_api:get(info).
+
+% @doc Identical to the docker version command.
+version() -> erldocker_api:get(version).
+
+%
+% Containers
+% http://docs.docker.io/en/latest/api/docker_remote_api_v1.4/#containers
+%
 
 % @doc Identical to the docker ps command.
-containers([{quiet, false}, {all, false}, {trunc, true}, {latest, false}, {since, undefined}, {before, undefined}, {limit, -1}] = Args) -> {error, not_implemented}.
+containers() -> containers(default_args(containers)).
+containers(Args) ->
+    erldocker_api:get([containers, ps], Args).
+
+% @doc Identical to the docker inspect command, but can only be used with a container ID.
+container(CID) ->
+    erldocker_api:get([containers, CID]).
+
+% @doc List processes running inside the container id.
+top(CID) ->
+    erldocker_api:get([containers, CID, top]).
+
+% @doc Identical to the docker diff command.
+changes(CID) ->
+    either:bind(erldocker_api:get([containers, CID, changes]), fun changes_proc/1).
+diff(CID) -> changes(CID).
+
+changes_proc(Changes) ->
+    changes_proc(Changes, []).
+changes_proc([{[{<<"Path">>,Path},{<<"Kind">>,Kind}]}|Rest], Acc) ->
+    changes_proc(Rest, [{Path, Kind}|Acc]);
+changes_proc([], Acc) ->
+    lists:reverse(Acc).
+
+% @doc Identical to the docker export command.
+export(CID) -> 
+    erldocker_api:get([containers, CID, export]).
+
+% @doc Start the container.
+start(CID, _Config) ->
+    erldocker_api:post([containers, CID, start]).
+
+% @doc Allows to bind a directory in the host to the container.
+% Similar to the docker run command with the -b="/host:/mnt".
+% Requires the container to be created with the volumes argument: c.create_container(..., volumes=[{'/mnt': {}}] = Args)
+% start_with_binds(Container, Binds) ->
+%     {error, not_implemented}.
+
+% @doc Stop the container.
+stop(CID) -> stop(CID, default_args(stop)).
+stop(CID, Args) ->
+    erldocker_api:post([containers, CID, stop], Args).
+
+% @doc Restart the container.
+restart(CID) -> restart(CID, default_args(restart)).
+restart(CID, Args) ->
+    erldocker_api:post([containers, CID, restart], Args).
+
+% @doc Identical to the docker kill command.
+kill(CID, _Config) ->
+    erldocker_api:post([containers, CID, kill]).
+
+% @doc Attach to container.
+attach(CID) -> attach(CID, default_args(attach)).
+attach(CID, Args) ->
+    % TODO: stream
+    erldocker_api:post([containers, CID, attach], Args).
+
+% @doc Identical to the docker rm command.
+delete(CID) -> delete(CID, default_args(delete)).
+delete(CID, Args) ->
+    erldocker_api:delete([containers, CID], Args).
+
+% @doc Identical to the docker wait command.
+wait(CID) ->
+    erldocker_api:post([containers, CID, wait]).
+
+% @doc Copy files or folders of container.
+copy(_CID, _Args) ->
+    {error, not_implemented}.
+
+%%%%% docker-py COPY-PASTE BELOW
 
 % @doc Creates a container that can then be started.
 % Parameters are similar to those for the docker run command except it doesn't support the attach options (-a)
 create_container(Image, Command, [{hostname, undefined}, {user, undefined}, {detach, false}, {stdin_open, false}, {tty, false}, {mem_limit, 0}, {ports, undefined}, {environment, undefined}, {dns, undefined}, {volumes, undefined}, {volumes_from, undefined}, {privileged, false}] = Args) -> {error, not_implemented}.
 
-% @doc Identical to the docker diff command.
-diff(Container) -> {error, not_implemented}.
+% @doc Similar to the docker build command.
+% path (required) can be a local path (To a directory containing a Dockerfile) or a remote URL
+build([{path, undefined}, {tag, undefined}, {quiet, false}, {nocache, false}] = Args) -> {error, not_implemented}.
 
-% @doc Identical to the docker export command.
-export(Container) -> {error, not_implemented}.
+% @doc Identical to the docker commit command.
+commit(Container, [{repository, undefined}, {tag, undefined}, {message, undefined}, {author, undefined}, {conf, undefined}] = Args) -> {error, not_implemented}.
 
 % @doc Identical to the docker history command.
 history(Image) -> {error, not_implemented}.
@@ -34,20 +112,11 @@ images([{name, undefined}, {quiet, false}, {all, false}, {viz, false}] = Args) -
 % To import an image from the local machine, src needs to be a file-like object or bytes collection.
 import_image(Src, [{repository, undefined}, {tag, undefined}] = Args) -> {error, not_implemented}.
 
-% @doc Identical to the docker info command.
-info() -> erldocker_api:get("/info").
-
 % @doc Identical to the docker insert command.
 insert(Url, Path) -> {error, not_implemented}.
 
-% @doc Identical to the docker inspect command, but can only be used with a container ID.
-inspect_container(Container_id) -> {error, not_implemented}.
-
 % @doc Identical to the docker inspect command, but can only be used with an image ID.
-inspect_image(Container_id) -> {error, not_implemented}.
-
-% @doc Identical to the docker kill command.
-kill(Containers) when is_list(Containers) -> {error, not_implemented}.
+inspect_image(ImageId) -> {error, not_implemented}.
 
 % @doc Identical to the docker login command (But non-interactive, obviously).
 login(Username, [{password, undefined}, {email, undefined}] = Args) -> {error, not_implemented}.
@@ -56,7 +125,7 @@ login(Username, [{password, undefined}, {email, undefined}] = Args) -> {error, n
 logs(Container) -> {error, not_implemented}.
 
 % @doc Identical to the docker port command.
-port(Container, Private_port) -> {error, not_implemented}.
+port(Container, PrivatePort) -> {error, not_implemented}.
 
 % @doc Identical to the docker pull command.
 pull(Repository, [{tag, undefined}, {registry, undefined}] = Args) -> {error, not_implemented}.
@@ -64,35 +133,49 @@ pull(Repository, [{tag, undefined}, {registry, undefined}] = Args) -> {error, no
 % @doc Identical to the docker push command.
 push(Repository) -> {error, not_implemented}.
 
-% @doc Identical to the docker rm command.
-remove_container(Containers, [{v, false}] = Args) when is_list(Containers) -> {error, not_implemented}.
-
 % @doc Identical to the docker rmi command.
-remove_image(Images) when is_list(Images) -> {error, not_implemented}.
-
-% @doc Identical to the docker restart command.
-restart(Containers, [{t, 10}] = Args) when is_list(Containers) -> {error, not_implemented}.
+remove_image(Image) -> {error, not_implemented}.
 
 % @doc Identical to the docker search command.
 search(Term) -> {error, not_implemented}.
 
-% @doc Identical to the docker start command, but doesn't support attach options.
-% Use docker logs to recover stdout/stderr
-start(Container) -> {error, not_implemented}.
-
-% @doc Allows to bind a directory in the host to the container.
-% )imilar to the docker run command with the -b="/host:/mnt".
-% Requires the container to be created with the volumes argument: c.create_container(..., volumes=[{'/mnt': {}}] = Args)
-start(Container, Binds) -> {error, not_implemented}.
-
-% @doc Identical to the docker stop command.
-stop(Containers, [{t, 10}] = Args) when is_list(Containers) -> {error, not_implemented}.
-
 % @doc Identical to the docker tag command.
 tag(Image, Repository, [{tag, undefined}, {force, false}] = Args) -> {error, not_implemented}.
 
-% @doc Identical to the docker version command.
-version() -> erldocker_api:get("/version").
+%%%%%%%%%%%%%
 
-% @doc Identical to the docker wait command.
-wait(Containers) when is_list(Containers) -> {error, not_implemented}.
+default_args(containers) ->
+    [{quiet, false}, {all, false}, trunc, {latest, false}, {since, undefined},
+        {before, undefined}, {limit, -1}];
+default_args(delete) ->
+    [{v, false}];
+default_args(restart) ->
+    [{t, 10}];
+default_args(stop) ->
+    [{t, 10}];
+default_args(attach) ->
+    [{logs, false}, {stream, false}, {stdin, false}, {stdout, false}, {stderr, false}];
+
+default_args(build) ->
+    [{path, undefined}, {tag, undefined}, {quiet, false}, {nocache, false}];
+default_args(commit) ->
+    [{repository, undefined}, {tag, undefined}, {message, undefined}, {author, undefined},
+        {conf, undefined}];
+default_args(create_container) ->
+    [{hostname, undefined}, {user, undefined}, {detach, false}, {stdin_open, false},
+        {tty, false}, {mem_limit, 0}, {ports, undefined}, {environment, undefined},
+        {dns, undefined}, {volumes, undefined}, {volumes_from, undefined}, {privileged, false}];
+default_args(images) ->
+    [{name, undefined}, {quiet, false}, {all, false}, {viz, false}];
+default_args(import_image) ->
+    [{repository, undefined}, {tag, undefined}];
+default_args(login) ->
+    [{password, undefined}, {email, undefined}];
+default_args(pull) ->
+    [{tag, undefined}, {registry, undefined}];
+default_args(tag) ->
+    [{tag, undefined}, {force, false}];
+
+default_args(_) ->
+    [].
+
--- src/erldocker_api.erl
+++ src/erldocker_api.erl
@@ -1,10 +1,62 @@
 -module(erldocker_api).
--compile([export_all]).
+-export([get/1, get/2, post/1, post/2, delete/1, delete/2]).
+
+-define(ADDR, <<"http://localhost:32133">>).
 -define(OPTIONS, [{pool, erldocker_pool}]).
 
-get(URL) ->
-    URL2 = "http://localhost:32133" ++ URL,
-    {ok, StatusCode, RespHeaders, Client} = hackney:get(URL2, [], <<>>, ?OPTIONS),
-    {ok, Body, Client1} = hackney:body(Client),
-    jiffy:decode(Body).
+get(URL) -> call(get, URL).
+get(URL, Args) -> call(get, URL, Args).
+post(URL) -> call(post, URL).
+post(URL, Args) -> call(post, URL, Args).
+delete(URL) -> call(delete, URL).
+delete(URL, Args) -> call(delete, URL, Args).
+
+call(Method, URL) when is_binary(URL) ->
+    error_logger:info_msg("api call: ~p ~s", [Method, binary_to_list(URL)]),
+    ReqHeaders = [{<<"Content-Type">>, <<"application/json">>}],
+    {ok, StatusCode, _RespHeaders, Client} = hackney:request(Method, URL, ReqHeaders, <<>>, ?OPTIONS),
+    {ok, Body, _Client1} = hackney:body(Client),
+    case StatusCode of
+        X when X == 200 orelse X == 201 orelse X == 204 ->
+            {ok, jiffy:decode(Body)};
+        _ ->
+            {error, {StatusCode, Body}}
+    end;
+call(Method, URL) ->
+    call(Method, to_url(URL)).
+call(Method, URL, Args) ->
+    call(Method, to_url(URL, Args)).
+
+argsencode([], Acc) ->
+    hackney_util:join(lists:reverse(Acc), <<"&">>);
+argsencode ([{_K,undefined}|R], Acc) ->
+    argsencode(R, Acc);
+argsencode ([{K,V}|R], Acc) ->
+    K1 = hackney_url:urlencode(to_binary(K)),
+    V1 = hackney_url:urlencode(to_binary(V)),
+    Line = << K1/binary, "=", V1/binary >>,
+    argsencode(R, [Line | Acc]);
+argsencode([K|R], Acc) ->
+    argsencode([{K, <<"true">>}|R], Acc).
+
+to_binary(X) when is_list(X) -> iolist_to_binary(X);
+to_binary(X) when is_atom(X) -> atom_to_binary(X, utf8);
+to_binary(X) when is_integer(X) -> list_to_binary(integer_to_list(X));
+to_binary(X) when is_binary(X) -> X.
+
+convert_url_parts(L) when is_list(L) -> convert_url_parts(L, []);
+convert_url_parts(X) -> convert_url_parts([X], []).
+
+convert_url_parts([X|Xs], Acc) ->
+    convert_url_parts(Xs, [<<"/", (to_binary(X))/binary>>|Acc]);
+convert_url_parts([], Acc) ->
+    lists:reverse(Acc).
+
+to_url(X) ->
+    iolist_to_binary([?ADDR|convert_url_parts(X)]).
+
+to_url(X, []) ->
+    to_url(X);
+to_url(X, Args) ->
+    iolist_to_binary([?ADDR, convert_url_parts(X), <<"?">>, argsencode(Args, [])]).
 
